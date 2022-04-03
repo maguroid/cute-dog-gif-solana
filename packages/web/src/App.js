cfc1f9c8985105db99818525a2fafd9d6c687cb0
maguro <maguro.devlife@gmail.com>
2022-04-03 14:27:02 +0900

    fix: anchor import path

--- packages/web/src/App.js
+++ packages/web/src/App.js
@@ -7,7 +7,7 @@ import { faThumbsUp } from "@fortawesome/free-solid-svg-icons";
 import { Connection, PublicKey, clusterApiUrl } from "@solana/web3.js";
 import { Program, Provider, web3 } from "@project-serum/anchor";
 
-import idl from "@gif-portal/anchor/target/idl/myepicproject.json";
+import idl from "@gif-portal/myepicproject/target/idl/myepicproject.json";
 import kp from "./keypair.json";
 
 const { SystemProgram, Keypair } = web3;
